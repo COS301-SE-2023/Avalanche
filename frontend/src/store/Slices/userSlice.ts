9d65150b2e5cd2f7cd1f9ebb4e90a885f25aedd6
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-05-18 10:49:27 +0200

    Interfaces, Requests, Responses, Enums

--- frontend/src/store/Slices/userSlice.ts
+++ frontend/src/store/Slices/userSlice.ts
@@ -3,28 +3,62 @@ import { AppState } from "../store";
 import { HYDRATE } from "next-redux-wrapper";
 
 export interface IUserState {
+    userID: string | null,
+    userData: IUser,
+    userAuth: IAuth
+}
+
+export interface IUser {
     username: string | null,
     email: string | null,
     firstName: string | null,
     lastName: string | null,
     profilePicture: string | null,
-    userID: string | number | null
+}
+
+export interface IAuth {
+    token: string | null,
 }
 
 const initialState: IUserState = {
-    username: null,
-    email: null,
-    firstName: null,
-    lastName: null,
-    profilePicture: null,
-    userID: null
+    userID: null,
+    userData: {
+        username: null,
+        email: null,
+        firstName: null,
+        lastName: null,
+        profilePicture: null,
+        userID: null
+    } as IUser,
+    userAuth: {
+        token: null
+    } as IAuth,
 }
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
+        setAuth(state) {
+            // set auth
+        },
+        getAuth(state) {
 
+        },
+        updateProfilePicture(state, action) {
+            // update profile picture
+        },
+        login(state, action) {
+            // handle login
+            const { email, password } = action.payload;
+        },
+        register(state, action) {
+            // handle register
+            const { email, confirmEmail, password, confirmPassword, firstName, lastName, country } = action.payload;
+        },
+        verifyOTP(state, action) {
+            const { email, token, otp } = action.payload;
+        }
     },
     extraReducers: (builder) => {
         builder.addCase(HYDRATE, (state, action) => {
@@ -36,6 +70,6 @@ export const userSlice = createSlice({
     }
 });
 
-export const { } = userSlice.actions;
+export const { setAuth, getAuth, updateProfilePicture, login, register, verifyOTP } = userSlice.actions;
 export const userState = (state: AppState) => state.user;
 export default userSlice.reducer;
