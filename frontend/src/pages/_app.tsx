2e519623a6dceee1a7ef46f6999131c6139852a9
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-05-18 04:22:22 +0200

    Redux State setup (with Persist)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,7 +2,8 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { wrapper } from '../store/store';
-import { Provider } from 'react-redux';
+import { Provider, } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Karla } from 'next/font/google';
 import 'animate.css';
 import '../assets/global.scss';
@@ -16,9 +17,11 @@ function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   return <ThemeProvider attribute="class">
     <Provider store={store}>
-      <main className={`${karla.variable} font-sans`}>
-        <Component {...props.pageProps} />
-      </main>
+      <PersistGate loading={null} persistor={store.__persistor}>
+        <main className={`${karla.variable} font-sans`}>
+          <Component {...props.pageProps} />
+        </main>
+      </PersistGate>
     </Provider>
   </ThemeProvider>
 }
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,16 +1,42 @@
-import { configureStore, ThunkAction, Action, applyMiddleware, MiddlewareArray } from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action, applyMiddleware, MiddlewareArray, combineReducers } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { modalManagerSlice } from "./modalManagerSlice";
 import { createWrapper } from "next-redux-wrapper";
 
-const makeStore = () =>
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+    [modalManagerSlice.name]: modalManagerSlice.reducer,
+});
+
+const makeConfiguredStore = () =>
     configureStore({
-        reducer: {
-            [modalManagerSlice.name]: modalManagerSlice.reducer,
-        },
+        reducer: rootReducer,
         middleware: [logger] as const,
         devTools: true
     });
+
+export const makeStore = () => {
+    const isServer = typeof window === "undefined";
+    if (isServer) {
+        return makeConfiguredStore();
+    } else {
+        const persistConfig = {
+            key: "nextjs",
+            whitelist: ["modalManager"],
+            storage
+        };
+        const persistedReducer = persistReducer(persistConfig, rootReducer);
+        let store: any = configureStore({
+            reducer: persistedReducer,
+            devTools: true,
+            middleware: [logger] as const,
+        });
+        store.__persistor = persistStore(store);
+        return store;
+    }
+}
 
 
 export type AppStore = ReturnType<typeof makeStore>;
