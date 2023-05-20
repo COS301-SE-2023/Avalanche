188af60e0145b9c572152d51bf4de6c6cb9d28ae
markbr02 <77788584+markbr02@users.noreply.github.com>
2023-05-20 12:02:32 +0200

    Deleted the backend nest instance
    We moved to a microservice arch

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
