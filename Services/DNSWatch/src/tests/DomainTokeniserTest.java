b73becb8cc4266d8328bc7535c4c6f38c46b0452
GT-GillesTeuwen <103629800+GT-GillesTeuwen@users.noreply.github.com>
2023-05-22 21:12:43 +0200

    Extended two word test

--- Services/DNSWatch/src/tests/DomainTokeniserTest.java
+++ Services/DNSWatch/src/tests/DomainTokeniserTest.java
@@ -43,6 +43,9 @@ public class DomainTokeniserTest {
     public void twoWordTest() {
         DomainTokeniser domainTokeniser = new DomainTokeniser();
         assertEquals("hithere", domainTokeniser.inferSpaces("hi there"));
+
+        assertEquals("helloyou", domainTokeniser.inferSpaces("hello you"));
+        assertEquals("coolguy", domainTokeniser.inferSpaces("cool guy"));
     }
 
     @Test
