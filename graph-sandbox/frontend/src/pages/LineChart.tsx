d02ac52976d5878ddcf56cd539d5674fcd91e5cf
Enchantress1416 <u21804312@tuks.co.za>
2023-05-22 21:22:59 +0200

    Added framework for linechart

---
+++ graph-sandbox/frontend/src/pages/LineChart.tsx
@@ -0,0 +1,9 @@
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+import React, { useRef, useEffect } from 'react';
+
+
+const labels = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+
+Chart.register(...registerables);
