fbcda4c08a8d3990dbf3f877f9b7ec64c349b789
Enchantress1416 <u21804312@tuks.co.za>
2023-05-22 21:16:58 +0200

    Added the AN dataset

--- graph-sandbox/frontend/src/pages/index.tsx
+++ graph-sandbox/frontend/src/pages/index.tsx
@@ -7,5 +7,12 @@ const labels = ["January","February","March","April","May","June","July","August
 
 const data = {
   labels: labels,
-  datasets: []
+  datasets: [{
+    label: 'AFRICA_NEW',
+    data: [932,1286,1220,858,935,839,760,802,802,894,847,701],
+    borderColor: 'rgba(54, 162, 235)', //blue
+    backgroundColor: 'rgba(54, 162, 235)',
+    borderWidth: 1,
+    yAxisID: 'y',
+  }]
 }
