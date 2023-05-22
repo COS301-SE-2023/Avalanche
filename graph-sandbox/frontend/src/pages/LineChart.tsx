051b32997f6de86acdb6ae2e9b824dd7d6699ee3
Enchantress1416 <u21804312@tuks.co.za>
2023-05-22 21:23:39 +0200

    Added data to line chart

--- graph-sandbox/frontend/src/pages/LineChart.tsx
+++ graph-sandbox/frontend/src/pages/LineChart.tsx
@@ -7,3 +7,50 @@ const labels = ["January","February","March","April","May","June","July","August
 
 
 Chart.register(...registerables);
+
+const data = {
+
+  labels: labels,
+
+  datasets: [
+  {
+    label: 'AFRICA_NEW',
+    data: [932,1286,1220,858,935,839,760,802,802,894,847,701],
+    borderColor: 'rgb(75, 192, 192)',
+    yAxisID: 'y',
+  },
+  {
+    label: 'AFRICA_PREMIUM',
+    data: [19,17,27,18,16,8,13,23,16,6,12,14],
+    borderColor: 'rgb(75, 192, 192)',
+    yAxisID: 'y',
+  },
+  {
+    label: 'AFRICA_TRANSFER',
+    data: [1147,1244,1147,1326,1785,2019,2691,2106,1298,1370,1155,1218],
+    borderColor: 'rgb(75, 192, 192)',
+    yAxisID: 'y',
+
+  },
+  {
+    label: 'AFRICA_TRANSFER',
+    data: [39,80,97,83,83,199,69,69,73,56,47,33],
+    borderColor: 'rgb(75, 192, 192)',
+    yAxisID: 'y',
+  },
+  {
+    label: 'AFRICA_CLOSED_REDEEM',
+    data: [6,5,0,40,19,36,29,22,15,22,24,25],
+    borderColor: 'rgb(75, 192, 192)',
+    yAxisID: 'y',
+  },
+  {
+    label: 'AFRICA_GRACE',
+    data: [5,10,5,5,4,5,3,4,5,12,6,3],
+    borderColor: 'rgb(75, 192, 192)',
+    yAxisID: 'y',
+  }
+
+  ]
+
+};
