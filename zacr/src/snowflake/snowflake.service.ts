2fdd478d83c7b33eeba5d61bcb5f30762e9d002f
Inge505 <u21689432@tuks.co.za>
2023-06-03 21:36:53 +0200

    Unit tested Snowflake Service :snowflake: :100:

--- zacr/src/snowflake/snowflake.service.ts
+++ zacr/src/snowflake/snowflake.service.ts
@@ -2,11 +2,21 @@ import { Inject, Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
 import { JwtService } from '@nestjs/jwt';
 @Injectable()
+/*
+    This filter will handle all connections to the Snowflake warehouse.
+    It will currently just execute the query given, which may be a call 
+    of a procedure or an SQL statement.
+    It could later be used to verify queries.
+*/
 export class SnowflakeService {
   constructor(
     @Inject('SNOWFLAKE_CONNECTION') private readonly snowflakeConnection,
   ) {}
 
+  /*
+  This function calls the query passed through as a parameter on 
+  the snowflake warehouse
+  */
   execute(query: string): Promise<any[]> {
     return new Promise((resolve, reject) => {
       this.snowflakeConnection.execute({
