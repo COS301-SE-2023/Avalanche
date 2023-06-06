d23bd3a0525ca63f720757e8d9ce40cec23562ff
markbr02 <77788584+markbr02@users.noreply.github.com>
2023-06-06 10:23:59 +0200

    rate limit added

--- gatekeeper/src/app.module.ts
+++ gatekeeper/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { ForwardService } from './forward.service';
 import { HttpModule } from '@nestjs/axios';
 import { ValidateRequestMiddleware } from './middleware/validate-request.middleware';
+import rateLimit from 'express-rate-limit';
 
 @Module({
   imports: [HttpModule],
@@ -14,7 +15,12 @@ import { ValidateRequestMiddleware } from './middleware/validate-request.middlew
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(ValidateRequestMiddleware) 
+      .apply(ValidateRequestMiddleware,
+        rateLimit({
+          windowMs: 60 * 1000, // 1 minute
+          max: 100, // limit each IP to 100 requests per windowMs
+          message: "{'error : Too many requests, please try again later.'}",
+        })) 
       .forRoutes('*');
   }
 }
