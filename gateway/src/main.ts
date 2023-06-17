4c90956ef849ad317acc1c63fc146ad8d6271a13
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-06-17 14:03:14 +0200

    Updates and Fixes

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
