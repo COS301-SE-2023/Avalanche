593fe84d72f5d6e54a0ac55e67b303747b22b664
markbr02 <77788584+markbr02@users.noreply.github.com>
2023-06-20 12:46:20 +0200

    domain watch working on gateway

--- gateway/src/domain-watch/domain-watch.service.ts
+++ gateway/src/domain-watch/domain-watch.service.ts
@@ -8,7 +8,7 @@ export class DomainWatchService {
   constructor(private httpService: HttpService) { }
 
   async sendData(data: any): Promise<any> {
-    const response = this.httpService.post('http://skunkworks.dns.net.za:4004/domainWatch/list', data);
+    const response = this.httpService.post('http://zanet.cloud:4004/domainWatch/list', data);
     const responseData = await lastValueFrom(response);
     return JSON.stringify(responseData.data);
   }
