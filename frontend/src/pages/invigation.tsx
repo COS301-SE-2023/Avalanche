8e2ccd3767e798d3bf2197f809cf0f2a29bc760d
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-06-22 17:50:53 +0200

    Invitation page

--- frontend/src/pages/invigation.tsx
+++
@@ -1,6 +0,0 @@
-import { useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/router';
-
-export default function Invitation() {
-    const searchParams = useSearchParams();
-}
