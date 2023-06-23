c75367d352b78796e109b3e863fc3db6fd9b0ed2
Inge505 <u21689432@tuks.co.za>
2023-06-24 01:29:43 +0200

    should not allow the last admin to exit the admin group

--- user-management/src/services/user-organisation-management-integration.service.spec.ts
+++ user-management/src/services/user-organisation-management-integration.service.spec.ts
@@ -577,6 +577,41 @@ describe('UserOrganisationMangementService Integration', () => {
       // Assert
       expect(result.status).toBe('success');
     }, 20000);
+
+    it('should not allow the last admin to exit the admin group', async () => {
+      // Arrange
+      const email = Random.email();
+      const password = Random.word(8);
+
+      const org = new Organisation();
+      org.name = Random.word(5);
+      const savedOrg = await organisationRepository.save(org);
+
+      const userGroup = new UserGroup();
+      userGroup.name = "admin-" + Random.word(5);
+      userGroup.permission = 1;
+      userGroup.organisation = savedOrg;
+      const savedUserGroup = await userGroupRepository.save(userGroup);
+
+      const user = new User();
+      user.email = email;
+      user.password = password;
+      user.organisation = savedOrg;
+      user.userGroups = [savedUserGroup];
+      await userRepository.save(user);
+
+      const jwtSecret = Random.word(10);
+      const jwtToken = jwt.sign({ email }, jwtSecret);
+      await redis.set(jwtToken, JSON.stringify(serializeUser(user)), 'EX', 24 * 60 * 60);
+
+      // Act
+      const result = await userOrganisationMangementService.exitUserGroup(jwtToken, savedUserGroup.name);
+
+      // Assert
+      expect(result.status).toBe(400);
+      expect(result.message).toBe('Cannot remove the last admin from the admin group');
+    }, 20000);
+    
   });
 
   describe('removeUserFromOrganisation', () => {
