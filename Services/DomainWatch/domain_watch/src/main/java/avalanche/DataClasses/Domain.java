d2eeac04df1991bc85961384cfdd5eb8c1b0f9d0
GT-GillesTeuwen <gillesteuwen@gmail.com>
2023-06-23 19:51:28 +0200

    Started javadoc comments

--- Services/DomainWatch/domain_watch/src/main/java/avalanche/DataClasses/Domain.java
+++ Services/DomainWatch/domain_watch/src/main/java/avalanche/DataClasses/Domain.java
@@ -7,6 +7,12 @@ public class Domain implements Comparable {
     private double similarity;
     private int metrics;
 
+    /**
+     * 
+     * @param name
+     * @param zone
+     * 
+     */
     public Domain(String name, String zone) {
         this.name = name;// .substring(0, name.length() - zone.length() - 1);
         this.zone = zone;
@@ -23,14 +29,42 @@ public class Domain implements Comparable {
         this.similarity = domain.similarity;
     }
 
+    /**
+     * @return String: returns the name of the domain
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return String: returns the zone of the domain
+     */
     public String getZone() {
         return zone;
     }
 
+    /**
+     * This method is used to update the total distance of this domain from some
+     * search string.
+     * 
+     * this.distance holds the total distance accross all metrics.
+     * 
+     * Different metrics add to the total distance in different ways.
+     * 
+     * Levenshtein: this metric adds to the distance as follows
+     * 1. if the length of this.name is greater than the distance then the total
+     * distance is increased by (length-distance)/length
+     * 2. if the length of this.name is less than or equal to the distance then the
+     * total distance is increased by (distance-length)/distance
+     * 
+     * Soundex: This metric adds to the distance as follows
+     * add 0.25 * distance to the total distance
+     * 
+     * We then add 1 to the number of metrics so that we can take an average later
+     * 
+     * @param distance the distance of this.name from a certain string
+     * @param metric   the metric that was used to calculate the above distance
+     */
     public void setDistance(double distance, String metric) {
         if (metric.equals("Levenshtein")) {
             if (this.name.length() > distance) {
@@ -45,7 +79,14 @@ public class Domain implements Comparable {
         metrics++;
     }
 
+    /**
+     * @return double: the average distance of this.name from a certain string
+     *         returns -1 if setDistance() was never called using any metrics
+     */
     public double getDistance() {
+        if (metrics == 0) {
+            return -1;
+        }
         return distance / metrics;
     }
 
@@ -70,6 +111,9 @@ public class Domain implements Comparable {
         return d.name.equals(this.name) && d.zone.equals(this.zone);
     }
 
+    /**
+     * @return String
+     */
     @Override
     public String toString() {
         return name + "." + zone.toLowerCase();
