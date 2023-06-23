528eecc2a8d238ab3a6df8f6cd2552eeb96860f1
Enchantress1416 <u21804312@tuks.co.za>
2023-06-23 22:37:56 +0200

    Added test to ensure integrateUserWithWExternalAPI is called correctly with the integrateUserWithWExternalAPI request

--- gateway/src/user-management/user-management.spec.ts
+++ gateway/src/user-management/user-management.spec.ts
@@ -208,6 +208,20 @@ describe('UserManagementService', () => {
         expect(clientProxy.send).toHaveBeenCalledWith({ cmd: 'removeUserFromOrganisation' }, data);
         expect(result).toBe(expectedResult)
     });
+
+    it('Usermanagement calls the correct proxy functions (integrateUserWithWExternalAPI)', async () => {
+        const data = { some: 'data' };
+        const expectedResult = { success: true };
+        const responseObservable = of(expectedResult);
+      
+        jest
+        .spyOn(clientProxy, 'send').mockImplementationOnce(() => responseObservable);
+      
+        const result = await service.integrateUserWithWExternalAPI(data);
+      
+        expect(clientProxy.send).toHaveBeenCalledWith({ cmd: 'integrateUserWithWExternalAPI' }, data);
+        expect(result).toBe(expectedResult)
+    });
       
 
 });
