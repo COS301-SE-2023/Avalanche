60d556f845c9b35a059ef0dcb7e43ec74e786b12
Enchantress1416 <u21804312@tuks.co.za>
2023-06-23 22:27:56 +0200

    Added test to ensure verify is called correctly with the verify request

--- gateway/src/user-management/user-management.spec.ts
+++ gateway/src/user-management/user-management.spec.ts
@@ -68,6 +68,20 @@ describe('UserManagementService', () => {
         expect(clientProxy.send).toHaveBeenCalledWith({ cmd: 'resendOTP' }, data);
         expect(result).toBe(expectedResult)
     });
+
+    it('Usermanagement calls the correct proxy functions (login)', async () => {
+        const data = { some: 'data' };
+        const expectedResult = { success: true };
+        const responseObservable = of(expectedResult);
+      
+        jest
+        .spyOn(clientProxy, 'send').mockImplementationOnce(() => responseObservable);
+      
+        const result = await service.login(data);
+      
+        expect(clientProxy.send).toHaveBeenCalledWith({ cmd: 'login' }, data);
+        expect(result).toBe(expectedResult)
+    });
       
 
 });
