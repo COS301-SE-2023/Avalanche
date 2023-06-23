831aa5a08696974195f5a700982615e904086b45
Enchantress1416 <u21804312@tuks.co.za>
2023-06-23 21:38:13 +0200

    Added test to ensure correct HHTPService service has been called in Domain Watch Service

--- gateway/src/domain-watch.spec.ts
+++ gateway/src/domain-watch.spec.ts
@@ -2,6 +2,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import {DomainWatchService} from './domain-watch/domain-watch.service'
 import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
 
 describe('domain watch service tests', () => {
     let domainWatchService: DomainWatchService;
@@ -26,4 +28,18 @@ describe('domain watch service tests', () => {
         expect(domainWatchService).toBeDefined();
     })
     
+    it("Domain watch sends valid data", async () => {
+        const data = { some: 'data' };
+      const responseData = { message: 'Success' };
+
+      jest.spyOn(httpService, 'post').mockReturnValueOnce(of({ data: responseData } as AxiosResponse));
+
+      const result = await domainWatchService.sendData(data);
+
+      expect(httpService.post).toHaveBeenCalledWith(
+        'http://zanet.cloud:4004/domainWatch/list',
+        data,
+      );
+      expect(result).toEqual(JSON.stringify(responseData));
+    })
 })
