3afbebb1e95488b4b71d7867f6e0daad8f004200
Inge505 <u21689432@tuks.co.za>
2023-06-24 16:40:08 +0200

    GetUser Invalid Token test

--- gateway/src/user-management/user-management-integration.spec.ts
+++ gateway/src/user-management/user-management-integration.spec.ts
@@ -185,5 +185,16 @@ describe('User Management Organisation Management Integration From Gateway', () 
           expect(response.body.message.email).toBe('test@test.com');
         });
     });
+
+    it('should not get user info if token is invalid', () => {
+      return request(app.getHttpServer())
+        .post('/user-management/getUserInfo')
+        .set('Authorization', 'Bearer invalid_token')
+        .expect(401)
+        .then((response) => {
+          console.log(response.body);
+          expect(response.body.message).toBe('JWT invalid');
+        });
+    });
   });
 });
