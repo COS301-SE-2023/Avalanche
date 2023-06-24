1923d3655e7cd1e0b5675c7a44ffc48f50de8d7e
Enchantress1416 <u21804312@tuks.co.za>
2023-06-24 11:05:02 +0200

    Added test to ensure transactions is called correctly with the transactions request

--- gateway/src/zacr/zacr.spec.ts
+++ gateway/src/zacr/zacr.spec.ts
@@ -2,6 +2,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientProxy, ClientProxyFactory } from '@nestjs/microservices';
 import { ZacrService } from './zacr.service';
+import { Observable, of } from 'rxjs';    
 
 describe('ZacrService', () => {
     let service: ZacrService;
@@ -25,5 +26,19 @@ describe('ZacrService', () => {
     it("Tests if service is defined", () => {
         expect(service).toBeDefined()
     })
+
+    it('Zacr calls the correct proxy functions (transactions)', async () => {
+        const data = { some: 'data' };
+        const expectedResult = { success: true };
+        const responseObservable = of(expectedResult);
+      
+        jest
+        .spyOn(clientProxy, 'send').mockImplementationOnce(() => responseObservable);
+      
+        const result = await service.transactions(data);
+      
+        expect(clientProxy.send).toHaveBeenCalledWith({ cmd: 'transactions' }, data);
+        expect(result).toBe(expectedResult)
+    });
 });
 
