52d2cdac59902e3669f916e690c103a97e7aabed
Inge505 <u21689432@tuks.co.za>
2023-06-24 20:43:52 +0200

    Dashboard Cypress

--- frontend/cypress/e2e/dashboard.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('Dashboard', () => {
-    beforeEach(() => {
-        cy.visit('/dashboard'); // Please replace with the actual route of your Dashboard page.
-    });
-    // Other tests go here...
-});
