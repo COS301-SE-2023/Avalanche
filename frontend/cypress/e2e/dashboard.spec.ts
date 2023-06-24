f25442378efc4b12be90b46ade5bb88d6a6434d7
Inge505 <u21689432@tuks.co.za>
2023-06-24 20:15:22 +0200

    Register tests Cypress

---
+++ frontend/cypress/e2e/dashboard.spec.ts
@@ -0,0 +1,6 @@
+describe('Dashboard', () => {
+    beforeEach(() => {
+        cy.visit('/dashboard'); // Please replace with the actual route of your Dashboard page.
+    });
+    // Other tests go here...
+});
--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -41,3 +41,57 @@ describe('Sign in Page', () => {
     cy.url().should('include', '/dashboard');
   });
 });
+
+describe('Register Test', () => {
+
+    beforeEach(() => {
+        // Visiting the register page before each test
+        cy.visit('http://localhost:3000/register');
+    });
+  
+    it('Should display validation error if emails do not match', () => {
+        cy.get('#name').type('Test');
+        cy.get('#surname').type('User');
+        cy.get('#email').type('test@test.com');
+        cy.get('#confirm-email').type('test1@test.com');
+        cy.get('#password').type('Password123');
+        cy.get('#confirm-password').type('Password123');
+        cy.get('#terms').check();
+  
+        cy.get('button[type="submit"]').click();
+  
+        cy.get('#toast-warning').should('exist');
+
+    });
+  
+    it('Should display validation error if passwords do not match', () => {
+        cy.get('#name').type('Test');
+        cy.get('#surname').type('User');
+        cy.get('#email').type('test@test.com');
+        cy.get('#confirm-email').type('test@test.com');
+        cy.get('#password').type('Password123');
+        cy.get('#confirm-password').type('Password124');
+        cy.get('#terms').check();
+  
+        cy.get('button[type="submit"]').click();
+  
+        cy.get('#toast-warning').should('exist');
+
+    });
+
+    it('Should display verification if all details are correct', () => {
+      cy.get('#name').type('Test');
+      cy.get('#surname').type('User');
+      cy.get('#email').type('test@test.com');
+      cy.get('#confirm-email').type('test@test.com');
+      cy.get('#password').type('Password123');
+      cy.get('#confirm-password').type('Password123');
+      cy.get('#terms').check();
+
+      cy.get('button[type="submit"]').click();
+
+      cy.contains('Verification'); 
+
+  })
+  });
+  
