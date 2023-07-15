d0e263f7e72ab81c7d2c63c76203ce1c67062e6e
Enchantress1416 <u21804312@tuks.co.za>
2023-07-15 19:08:31 +0200

    setup of testing for user-data-products-management-service

---
+++ user-management/src/services/user-data-products-management.service.spec.ts
@@ -0,0 +1,80 @@
+/* eslint-disable prettier/prettier */
+
+import { Organisation } from "src/entity/organisation.entity";
+import { User } from "src/entity/user.entity";
+import { UserGroup } from "src/entity/userGroup.entity";
+import { Repository } from "typeorm";
+import { UserDataProductMangementService } from "./user-data-products-management.service";
+import Redis from "ioredis";
+import { ConfigService } from "@nestjs/config";
+import { TestingModule, Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+describe('UserDataProductMangementService', async () => {
+    let userDataProductMangementService: UserDataProductMangementService; //service we are testing
+    //the belwo are the repos we are going to be using
+    let mockUserRepository: jest.Mocked<Partial<Repository<User>>>;
+    let mockUserGroupRepository: jest.Mocked<Partial<Repository<UserGroup>>>;
+    let mockOrganisationRepository: jest.Mocked<Partial<Repository<Organisation>>>;
+    let mockRedis: jest.Mocked<Redis>;
+
+    beforeEach(async () => {
+        const userRepository = {
+          findOne: jest.fn(),
+          create: jest.fn(),
+          save: jest.fn(),
+        };
+    
+        const userGroupRepository = {
+          findOne: jest.fn(),
+          create: jest.fn(),
+          save: jest.fn(),
+        };
+    
+    
+        const organisationRepository = {
+          findOne: jest.fn(),
+          create: jest.fn(),
+          save: jest.fn(),
+        };
+    
+        const redis = {
+          get: jest.fn(),
+          set: jest.fn(),
+          del: jest.fn(),
+        };
+    
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [
+            UserDataProductMangementService,
+            {
+              provide: getRepositoryToken(User),
+              useValue: userRepository,
+            },
+            {
+              provide: getRepositoryToken(UserGroup),
+              useValue: userGroupRepository,
+            },
+            {
+              provide: getRepositoryToken(Organisation),
+              useValue: organisationRepository,
+            },
+            {
+              provide: 'REDIS', // Provide the mock redis service
+              useValue: redis,
+            },
+            ConfigService
+          ],
+        }).compile();
+    
+        userDataProductMangementService = module.get<UserDataProductMangementService>(UserDataProductMangementService);
+        mockUserRepository = module.get(getRepositoryToken(User));
+        mockUserGroupRepository = module.get(getRepositoryToken(UserGroup));
+        mockOrganisationRepository = module.get(getRepositoryToken(Organisation));
+        //UserOrganisationMangementService.prototype.sendRegistrationEmail = mockSendRegistrationEmail;
+        //UserOrganisationMangementService.prototype.sendInvitationEmail = mockSendInvitationEmail;
+        mockRedis = module.get('REDIS');
+      });
+
+      
+})
