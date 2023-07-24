938078aab27f671d40b47440c325764ef2d9026f
markbr02 <77788584+markbr02@users.noreply.github.com>
2023-07-24 19:53:41 +0200

    error fixes

--- ryce/src/app.controller.ts
+++ ryce/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, RpcException } from '@nestjs/microservices';
 import { TransactionService } from './transactions/transactions.service';
 import { MarketShareService } from './marketShare/marketShare.service';
 import { AgeService } from './age/age.service';
@@ -21,57 +21,129 @@ export class AppController {
   @MessagePattern({ cmd: 'transactions' })
   async transactions(data: any) {
     console.log('Transactions: ', data);
-    return await this.transactionsService.transactions(
+    const result =  await this.transactionsService.transactions(
       data.filters,
       data.graphName,
     );
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 
   @MessagePattern({ cmd: 'transactions-ranking' })
   async transactionsRanking(data: any) {
     console.log('Transactions: ', data);
-    return await this.transactionsService.transactionsRanking(
+    const result = await this.transactionsService.transactionsRanking(
       data.filters,
       data.graphName,
     );
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 
   @MessagePattern({ cmd: 'marketShare' })
   async marketShare(data: any) {
-    return await this.marketShareService.marketShare(
+    const result = await this.marketShareService.marketShare(
       data.filters,
       data.graphName,
     );
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 
   @MessagePattern({ cmd: 'age' })
   async age(data: any) {
-    return await this.ageService.age(data.filters, data.graphName);
+    const result = await this.ageService.age(data.filters, data.graphName);
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 
   @MessagePattern({ cmd: 'domainNameAnalysis/count' })
   async domainNameAnalysisCount(data: any) {
-    return await this.domainNameAnalysisService.sendData(data);
+    const result = await this.domainNameAnalysisService.sendData(data);
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 
   @MessagePattern({ cmd: 'domainNameAnalysis/length' })
   async domainNameAnalysisLength(data: any) {
-    return await this.domainNameAnalysisService.domainLength(
+    const result = await this.domainNameAnalysisService.domainLength(
       data.filters,
       data.graphName,
     );
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 
   @MessagePattern({ cmd: 'movement/vertical' })
   async nettVerticalMovement(data: any) {
-    return await this.movementService.nettVeritical(
+    const result = await this.movementService.nettVeritical(
       data.filters,
       data.graphName,
     );
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 
   @MessagePattern({ cmd: 'registrarName' })
   async registarName(data: any) {
-    return await this.registrarNameService.registrarName(data);
+    const result = await this.registrarNameService.registrarName(data);
+    if (result.error) {
+      throw new RpcException({
+        status: result.status,
+        message: result.message,
+        timestamp: result.timestamp,
+      });
+    }
+
+    return result;
   }
 }
