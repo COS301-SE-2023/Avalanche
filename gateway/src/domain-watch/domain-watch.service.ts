bb55877c154089ac6f331736975c2c53086dfdfe
Inge505 <u21689432@tuks.co.za>
2023-07-25 16:01:10 +0200

    Filter update

--- gateway/src/domain-watch/domain-watch.service.ts
+++ gateway/src/domain-watch/domain-watch.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
@@ -6,11 +5,14 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class DomainWatchService {
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {}
 
   async sendData(data: any): Promise<any> {
     console.log(data);
-    const response = this.httpService.post('http://zanet.cloud:4100/domainWatch/active', data);
+    const response = this.httpService.post(
+      'http://zanet.cloud:4100/domainWatch/active',
+      data,
+    );
     const responseData = await lastValueFrom(response);
     return JSON.stringify(responseData.data);
   }
@@ -18,27 +20,46 @@ export class DomainWatchService {
   @Cron(CronExpression.EVERY_DAY_AT_6AM)
   async passive() {
     console.log('Running every day at midnight');
-    const userData  = await this.httpService.post('http://localhost:4000/user-management/getDomainWatchPassive').toPromise();
+    const userData = await this.httpService
+      .post('http://localhost:4000/user-management/getDomainWatchPassive')
+      .toPromise();
     const userInfo = userData.data.watched;
-    const africaData = await this.httpService.post('http://localhost:4000/africa/domainWatchPassive').toPromise();
+    const africaData = await this.httpService
+      .post('http://localhost:4000/africa/domainWatchPassive')
+      .toPromise();
     const africaInfo = africaData.data.queryData[0]['DOMAINWATCHPASSIVE'];
-    const check = {'watched' : userInfo , 'recently-created' : africaInfo};
-    const response = await this.httpService.post('http://zanet.cloud:4100/domainWatch/passive', check).toPromise();
+    const check = { watched: userInfo, 'recently-created': africaInfo };
+    const response = await this.httpService
+      .post('http://zanet.cloud:4100/domainWatch/passive', check)
+      .toPromise();
     return JSON.stringify(response.data);
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_7AM)
   async loadDomains() {
     console.log('Running every day');
-    const africaData = await this.httpService.post('http://localhost:4000/africa/loadDomains').toPromise();
+    const africaData = await this.httpService
+      .post('http://localhost:4000/africa/loadDomains')
+      .toPromise();
     const africaInfo = africaData.data.queryData[0]['LOADDOMAINS'];
-    const zacrData = await this.httpService.post('http://localhost:4000/zacr/loadDomains').toPromise();
+    const zacrData = await this.httpService
+      .post('http://localhost:4000/zacr/loadDomains')
+      .toPromise();
     const zacrInfo = zacrData.data.queryData[0]['LOADDOMAINS'];
-    const ryceData = await this.httpService.post('http://localhost:4000/africa/loadDomains').toPromise();
+    const ryceData = await this.httpService
+      .post('http://localhost:4000/africa/loadDomains')
+      .toPromise();
     const ryceInfo = ryceData.data.queryData[0]['LOADDOMAINS'];
-    const check = {'domains' : [{'name': 'AFRICA_domains','domains':africaInfo}, {'name': 'ZACR_domains','domains':zacrInfo}, {'name': 'RYCE_domains','domains':ryceInfo}]};
-    const response = await this.httpService.post('http://zanet.cloud:4100/domainWatch/loadDomains', check).toPromise();
+    const check = {
+      registryDomains: [
+        { registryName: 'AFRICA_Domains', domains: africaInfo },
+        { registryName: 'ZACR_Domains', domains: zacrInfo },
+        { registryName: 'RYCE_Domains', domains: ryceInfo },
+      ],
+    };
+    const response = await this.httpService
+      .post('http://zanet.cloud:4100/domainWatch/loadDomains', check)
+      .toPromise();
     return JSON.stringify(response.data);
   }
 }
-
