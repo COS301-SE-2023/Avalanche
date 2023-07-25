9db1c407ce86b8b9455ada36e5cfe81f571251b9
markbr02 <77788584+markbr02@users.noreply.github.com>
2023-07-25 12:48:56 +0200

    we got who is

--- gateway/src/domain-watch/domain-watch.service.ts
+++ gateway/src/domain-watch/domain-watch.service.ts
@@ -3,6 +3,7 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import * as whois from 'node-whois';
 
 @Injectable()
 export class DomainWatchService {
@@ -18,11 +19,11 @@ export class DomainWatchService {
   @Cron(CronExpression.EVERY_DAY_AT_6AM)
   async passive() {
     console.log('Running every day at midnight');
-    const userData  = await this.httpService.post('http://localhost:4000/user-management/getDomainWatchPassive').toPromise();
+    const userData = await this.httpService.post('http://localhost:4000/user-management/getDomainWatchPassive').toPromise();
     const userInfo = userData.data.watched;
     const africaData = await this.httpService.post('http://localhost:4000/africa/domainWatchPassive').toPromise();
     const africaInfo = africaData.data.queryData[0]['DOMAINWATCHPASSIVE'];
-    const check = {'watched' : userInfo , 'recently-created' : africaInfo};
+    const check = { 'watched': userInfo, 'recently-created': africaInfo };
     const response = await this.httpService.post('http://zanet.cloud:4100/domainWatch/passive', check).toPromise();
     return JSON.stringify(response.data);
   }
@@ -36,9 +37,21 @@ export class DomainWatchService {
     const zacrInfo = zacrData.data.queryData[0]['LOADDOMAINS'];
     const ryceData = await this.httpService.post('http://localhost:4000/africa/loadDomains').toPromise();
     const ryceInfo = ryceData.data.queryData[0]['LOADDOMAINS'];
-    const check = {'domains' : [{'name': 'AFRICA_domains','domains':africaInfo}, {'name': 'ZACR_domains','domains':zacrInfo}, {'name': 'RYCE_domains','domains':ryceInfo}]};
+    const check = { 'domains': [{ 'name': 'AFRICA_domains', 'domains': africaInfo }, { 'name': 'ZACR_domains', 'domains': zacrInfo }, { 'name': 'RYCE_domains', 'domains': ryceInfo }] };
     const response = await this.httpService.post('http://zanet.cloud:4100/domainWatch/loadDomains', check).toPromise();
     return JSON.stringify(response.data);
+  }
+
+  async whoisyou(data: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      whois.lookup(data.domain, function(err, data) {
+          if (err) {
+              reject(err);
+          } else {
+              resolve({"data" : data});
+          }
+      });
+  });
   }
 }
 
