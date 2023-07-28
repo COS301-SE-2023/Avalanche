665c70a99b1683cd183ba89f5c46e11843bc1785
Enchantress1416 <u21804312@tuks.co.za>
2023-07-28 14:15:46 +0200

    incorrect pswd and incorrect email

---
+++ frontend/cypress/e2e/login.spec.cy.ts
@@ -0,0 +1,27 @@
+describe('login', () =>{
+    beforeEach(() =>{
+        cy.visit('http://localhost:3000');
+    })
+
+    /*it('Signin with nothing completed', () => {
+        cy.get('button[type=submit]').click(); // Please replace with the actual route of your Dashboard page.
+        cy.get('#toast-warning').should('exist');
+    })*/
+
+    it('Incorrect email and password', () =>{
+        //given
+        cy.get('input[name=email]').type('fake@fakier.com');
+        cy.get('input[name=password]').type('fakePassword');
+        cy.intercept("POST", "user-management/login").as("response")
+
+        //when
+        cy.get('button[type=submit]').click(); // Please replace with the actual route of your Dashboard page.
+        
+        //then
+        cy.wait("@response").then((interception) => {
+            const result = interception.response
+            expect(result.statusCode).to.equal(400)
+            expect(result.body.message).to.equal("This user does not exist, please enter the correct email/please register.")
+        })
+    })
+})
