ee8d0025769a2c23bac996d775e90ff2906a792a
Enchantress1416 <u21804312@tuks.co.za>
2023-07-28 15:43:07 +0200

    ensuring can still see navbar while in settings page

---
+++ frontend/cypress/e2e/settings.spec.cy.ts
@@ -0,0 +1,32 @@
+describe("Settings", () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000');
+        cy.get('input[name=email]').type('kihale5691@sportrid.com');
+        cy.get('input[name=password]').type('12345');
+        cy.get('button[type=submit]').click(); // Please replace with the actual route of your Dashboard page.
+        cy.get('a[data-tooltip-target="tooltip-settings"].inline-flex.justify-center.p-2.text-black.rounded.cursor-pointer.dark\\:text-white.dark\\:hover\\:text-white.hover\\:text-gray-900.hover\\:bg-gray-100.dark\\:hover\\:bg-gray-600[href="/settings"]').click();
+    });
+
+    it("Navbar still visible", ()=> {
+        function navBarChecks(href, contain){
+            cy.get('#default-sidebar a[href="'+href+'"]')
+                .should('be.visible')
+                .and('contain', contain);
+        }
+
+        cy.url().should("include", "/settings")
+
+        cy.get('#default-sidebar')
+            .should('be.visible');
+
+        navBarChecks("/dashboard", "Home")
+        navBarChecks("/registrar", "Registrar")
+        navBarChecks("/registrarMarketComparison", "Registrar Market Comparison")
+        navBarChecks("/movement", "Movement")
+        navBarChecks("/domainLength", "Domain Length")
+        navBarChecks("/marketShare", "Market Share")
+        navBarChecks("/ageAnalysis", "Registrar Age Analysis")
+        navBarChecks("/domainNameAnalysis", "Domain Name Analysis")
+        navBarChecks("/watch", "Domain Watch")
+    })
+})
