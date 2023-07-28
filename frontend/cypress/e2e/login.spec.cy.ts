065430b8349cbc52a94941b93b19e6fa68310eda
Enchantress1416 <u21804312@tuks.co.za>
2023-07-28 16:39:15 +0200

    Moved tests from spec to login

--- frontend/cypress/e2e/login.spec.cy.ts
+++ frontend/cypress/e2e/login.spec.cy.ts
@@ -1,3 +1,35 @@
+describe('Sign in Page', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000') ;
+    });
+  
+    it('successfully loads', () => {
+      cy.contains('Avalanche Analytics'); 
+    });
+  
+    it('should have email input', () => {
+      cy.get('input[name=email]')
+        .should('have.attr', 'placeholder', 'name@company.com')
+        .should('have.value', '');
+    });
+  
+    it('should have password input', () => {
+      cy.get('input[name=password]')
+        .should('have.attr', 'placeholder', '••••••••')
+        .should('have.value', '');
+    });
+  
+    it('should navigate to "Forgot password" page on clicking "Forgot password"', () => {
+      cy.get('a[href="/forgot"]').click();
+      cy.url().should('include', '/forgot');
+    });
+  
+    it('should navigate to "Register" page on clicking "Sign up"', () => {
+      cy.contains("Sign up").click();
+      cy.url().should('include', '/register');
+    });
+  });
+
 describe('login', () =>{
     beforeEach(() =>{
         cy.visit('http://localhost:3000');
--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,46 +1,4 @@
-describe('Sign in Page', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000') ;
-  });
 
-  it('successfully loads', () => {
-    cy.contains('Avalanche Analytics'); 
-  });
-
-  it('should have email input', () => {
-    cy.get('input[name=email]')
-      .should('have.attr', 'placeholder', 'name@company.com')
-      .should('have.value', '');
-  });
-
-  it('should have password input', () => {
-    cy.get('input[name=password]')
-      .should('have.attr', 'placeholder', '••••••••')
-      .should('have.value', '');
-  });
-
-  it('should navigate to "Forgot password" page on clicking "Forgot password"', () => {
-    cy.get('a[href="/forgot"]').click();
-    cy.url().should('include', '/forgot');
-  });
-
-  it('should navigate to "Register" page on clicking "Sign up"', () => {
-    cy.contains("Sign up").click();
-    cy.url().should('include', '/register');
-  });
-
-  // Assuming you have a valid user in your application with the following credentials:
-  // Email: 'valid.user@example.com'
-  // Password: 'validpassword'
-  it('should login with valid credentials', () => {
-    cy.get('input[name=email]').type('ingeodendaal5@gmail.com');
-    cy.get('input[name=password]').type('password');
-    cy.get('button[type=submit]').click();
-
-    // Assuming that successful login redirects to '/dashboard'
-    cy.url().should('include', '/dashboard');
-  });
-});
 
 describe('Register Test', () => {
 
@@ -94,4 +52,100 @@ describe('Register Test', () => {
 
   })
   });
+
+const baseURL = 'http://localhost:3000/';
+const server = 'http://localhost:4000/'
+
+//require('cypress-dotenv')();
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe('Home page test', () => {
+  beforeEach(() => {
+    cy.visit(baseURL);
+  })
+
+  it('should return a successful response', () => {
+    cy.request(baseURL).its("status")
+      .should('equal', 200);
+  });
+
+  it('ensures all elements exist', () => {
+    cy.get('body').should('exist');
+    cy.get('script').should('exist');
+    cy.get('#__next').should('exist');
+    cy.get('#__NEXT_DATA__').should('exist');
+  });
+
+  it('should have specific headers', () => {
+    cy.request(baseURL)
+      .its('headers')
+      .should('include', {
+        'content-type': 'text/html; charset=utf-8',
+      });
+  });
+})
+
+describe('User management', () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+  describe('login', ()=>{
+    /*it('Endpoint is reachable', async() => {
+      cy.request({
+        method: 'GET',
+        url: server + 'user-management/login',
+        failOnStatusCode: false
+      }).its('status').should('equal', 400)
+    })*/
+
+    it('Email and password is empty', async() => {
+      const requestBody = {
+        email : "",
+        password : ""
+      };
+
+      cy.request({
+        method: 'POST',
+        url : "127.0.0.1:4000/user-management/login",
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: requestBody,
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.equal(400);
+      })
+    })
+
+    it('Email valid but password missing',async () => {
+      //given
+      const requestBody = {
+        email : "u21804312@tuks.co.za",
+        password : ""
+      };
+      cy.log(Cypress.env('email'))
+
+      //when
+      cy.request({
+        method: 'POST',
+        url: server + "user-management/login",
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: requestBody,
+        failOnStatusCode: false
+      }).then((response) => {
+        
+        expect(response.status).to.equal(400)
+        expect(response.body.message).to.equal("Incorrect password")
+      })
+    })
+  })
+})
   
