9c4998528649b612a56c9acc179cba74d5049cb0
Enchantress1416 <u21804312@tuks.co.za>
2023-07-29 20:54:35 +0200

    nowwwww logout works from dashboards lol

--- frontend/cypress/e2e/dashboard.spec.cy.ts
+++ frontend/cypress/e2e/dashboard.spec.cy.ts
@@ -48,7 +48,15 @@ describe('Dashboard', () => {
 
         cy.get('path[fill-rule="evenodd"][d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z"]')
              .click()
-           
+        cy.contains('Sign in to your account').should('be.visible')
+        cy.contains('Your email').should('be.visible')
+        cy.get('input[placeholder="name@company.com"]').should('be.visible');
+        cy.contains('Password').should('be.visible')
+        cy.get('input[placeholder="••••••••"]').should('be.visible');
+        cy.contains('Forgot password').should('be.visible')
+        cy.contains('Don\'t have an account yet?').should('be.visible')
+        cy.contains('Sign in').should('be.visible')
+        cy.contains('Avalanche Analytics').should('be.visible')
 
     })
 });
