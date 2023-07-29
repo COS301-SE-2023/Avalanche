24351d81b7a848dddc6deadcb069fdbe644b0be6
Inge505 <u21689432@tuks.co.za>
2023-07-29 21:05:32 +0200

    registrarRanking test

--- zacr/src/transactions/transactions.service.ts
+++ zacr/src/transactions/transactions.service.ts
@@ -74,6 +74,7 @@ export class TransactionService {
   async transactionsRanking(filters: any, graphName: string): Promise<any> {
     try {
       graphName = this.transactionsGraphName(filters, true);
+      console.log(graphName);
       const filterObj = JSON.parse(JSON.stringify(filters));
       filterObj.isRanking = true;
       filters = JSON.stringify(filterObj);
@@ -94,7 +95,7 @@ export class TransactionService {
             timestamp: new Date().toISOString(),
           };
         }
-        console.log(queryData);
+
         formattedData =
           await this.graphFormattingService.formatTransactionsRanking(
             JSON.stringify(queryData),
@@ -175,18 +176,17 @@ export class TransactionService {
     }
 
     let reg = '';
+    let trans = ' Transactions ';
     if (perReg) {
       reg = 'per registrar ';
+      console.log('WEEEEEEE');
+      console.log(filters['transactions']);
+      if (filters['transactions']?.length > 0) {
+        trans = ' ' + filters['transaction'].join(', ') + ' ';
+      }
     }
     return (
-      granularity +
-      ' Transactions ' +
-      reg +
-      ' from ' +
-      dateFrom +
-      ' to ' +
-      dateTo +
-      zone
+      granularity + trans + reg + ' from ' + dateFrom + ' to ' + dateTo + zone
     );
   }
 }
