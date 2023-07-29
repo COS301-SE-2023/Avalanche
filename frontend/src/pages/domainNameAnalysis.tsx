0f0aa9d11378b8b9335e8fb135bcb0544786df79
Inge505 <u21689432@tuks.co.za>
2023-07-29 19:31:41 +0200

    Fixed icons and headers, cause I'm also frontend engineer now...

--- frontend/src/pages/domainNameAnalysis.tsx
+++ frontend/src/pages/domainNameAnalysis.tsx
@@ -1,6 +1,6 @@
 import Sidebar from "@/components/Navigation/SideBar"
 import PageHeader from "@/components/Util/PageHeader"
-import { HomeIcon } from "@heroicons/react/24/solid"
+import { EyeIcon } from "@heroicons/react/24/solid"
 import Head from "next/head"
 import { ChartCard } from "@/components/Graphs"
 import { ChartType } from "@/Enums";
@@ -53,7 +53,7 @@ export default function DomainNameAnalysis() {
 
         <div className="p-4 sm:ml-64 bg-gray-100 dark:bg-secondaryBackground min-h-screen">
             <div className="flex justify-between items-center">
-                <PageHeader title="Domain Name Analysis" subtitle="Insights at your fingertips" icon={<HomeIcon className="h-16 w-16 text-black dark:text-white" />} />
+                <PageHeader title="Domain Name Analysis" subtitle="Insights at your fingertips" icon={<EyeIcon className="h-16 w-16 text-black dark:text-white" />} />
             </div>
             <div className="p-0 pt-4 md:p-4">
                 <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-4 mb-4 grid-rows-2">
