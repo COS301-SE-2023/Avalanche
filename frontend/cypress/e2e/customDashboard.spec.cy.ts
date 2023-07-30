62c40204fd30d874fef1f4d2ca97e948235f1e74
Inge505 <u21689432@tuks.co.za>
2023-07-30 13:59:21 +0200

    repkaced url

--- frontend/cypress/e2e/customDashboard.spec.cy.ts
+++ frontend/cypress/e2e/customDashboard.spec.cy.ts
@@ -1,6 +1,6 @@
 describe('Custom Dashboard', () => {
     before(() => {
-        cy.visit('http://localhost:3000');
+        cy.visit(Cypress.env('baseURL')+':'+Cypress.env('basePort'));
         cy.get('input[name=email]').type(Cypress.env('username'));
         cy.get('input[name=password]').type(Cypress.env('password'));
         cy.get('button[type=submit]').click(); 
@@ -13,7 +13,7 @@ describe('Custom Dashboard', () => {
         cy.get('input[name=name]').type(dashboardName);
         cy.get('button[type=submit]').contains('Create Dashboard').click();
         
-        cy.url().should('include', `http://localhost:3000/custom/`);
+        cy.url().should('include', Cypress.env('baseURL')+':'+Cypress.env('basePort') + '/custom/');
         cy.url().should('include', `?name=${encodeURIComponent(dashboardName)}`);
     });
 });
