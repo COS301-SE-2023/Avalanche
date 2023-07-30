895af572a65bc3f9b112da45d5384b0ef24aa24a
Inge505 <u21689432@tuks.co.za>
2023-07-30 12:46:46 +0200

    Replaced hardcoded url with config call

--- frontend/cypress/e2e/dashboard.spec.cy.ts
+++ frontend/cypress/e2e/dashboard.spec.cy.ts
@@ -1,28 +1,28 @@
 describe('Dashboard', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000');
-        cy.get('input[name=email]').type('ingeodendaal5@gmail.com');
-    cy.get('input[name=password]').type('password');
-    cy.get('button[type=submit]').click(); // Please replace with the actual route of your Dashboard page.
+        cy.visit(Cypress.env('baseURL') + ":" + Cypress.env('basePort'));
+        cy.get('input[name=email]').type(Cypress.env('username'));
+        cy.get('input[name=password]').type(Cypress.env('password'));
+        cy.get('button[type=submit]').click(); // Please replace with the actual route of your Dashboard page.
     });
-    
+
     it('renders Sidebar with links', () => {
         it('The navigation should be visible', () => {
             cy.get('#default-sidebar')
-              .should('be.visible');
-          });
-        
-          it('Home link is visible', () => {
+                .should('be.visible');
+        });
+
+        it('Home link is visible', () => {
             cy.get('#default-sidebar a[href="/dashboard"]')
-              .should('be.visible')
-              .and('contain', 'Home');
-          });
+                .should('be.visible')
+                .and('contain', 'Home');
+        });
     });
 
     it('renders page header', () => {
         cy.get('h1') // Assuming the PageHeader component renders a <header> element.
             .should('be.visible');
-    
+
     });
-    
+
 });
