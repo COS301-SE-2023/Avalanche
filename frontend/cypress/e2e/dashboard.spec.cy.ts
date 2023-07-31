3e77a4912e6cda398da82d1309267508dae5deef
Inge505 <u21689432@tuks.co.za>
2023-07-31 15:40:37 +0200

    Dashboard ... Hopefully

--- frontend/cypress/e2e/dashboard.spec.cy.ts
+++ frontend/cypress/e2e/dashboard.spec.cy.ts
@@ -1,9 +1,11 @@
 describe('Dashboard', () => {
     beforeEach(() => {
-        cy.visit(Cypress.env('baseURL') + ":" + Cypress.env('basePort'));
-        cy.get('input[name=email]').type(Cypress.env('username'));
-        cy.get('input[name=password]').type(Cypress.env('password'));
-        cy.get('button[type=submit]').click(); // Please replace with the actual route of your Dashboard page.
+        // cy.visit(Cypress.env('baseURL') + ":" + Cypress.env('basePort'));
+        // cy.get('input[name=email]').type(Cypress.env('username'));
+        // cy.get('input[name=password]').type(Cypress.env('password'));
+        // cy.get('button[type=submit]').click(); // Please replace with the actual route of your Dashboard page.
+        cy.setCookie('jwt', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzQGdtYWlsLmNvbSIsImlhdCI6MTY5MDQ3NzU2MCwiZXhwIjoxNjkwNTYzOTYwfQ.3iHbfNq3HjLmDYu49wzsvryciHyiOgvTrL67Bcr6yWM');
+        cy.visit(Cypress.env('baseURL') + ":" + Cypress.env('basePort') + '/dashboard');
         cy.wait(20000);
     });
 
