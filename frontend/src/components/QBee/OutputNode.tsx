96092a45d7c1b7ec9f53f1d3f58bbbfe9a86876c
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-08-15 20:35:09 +0200

    :bee:

---
+++ frontend/src/components/QBee/OutputNode.tsx
@@ -0,0 +1,91 @@
+import { useCallback } from 'react';
+import { Node, NodeProps, Handle, Position } from 'reactflow';
+
+type NodeData = {
+    title: string
+};
+
+type CustomNode = Node<NodeData>;
+
+export default function CustomNode({ data }: NodeProps<NodeData>) {
+
+    const handles = [
+        "select", "filter", "group", "choose"
+    ]
+
+    const renderHandle = () => {
+        return handles.map((item: string, index: number) => <Handle type="target" position={Position.Left} id={item} style={{ backgroundColor: "#121212", top: 10 + (index * 15), width: "10px", borderRadius: "0px", height: "10px" }} />)
+    }
+
+    return <div className='bg-avalancheBlue p-3 pl-5 rounded'>
+        {renderHandle()}
+        <div>
+            <span className='text-xl'>Results</span>
+            <div className="relative overflow-x-auto rounded">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Product name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Color
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Price
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">
+                                Silver
+                            </td>
+                            <td className="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td className="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Microsoft Surface Pro
+                            </th>
+                            <td className="px-6 py-4">
+                                White
+                            </td>
+                            <td className="px-6 py-4">
+                                Laptop PC
+                            </td>
+                            <td className="px-6 py-4">
+                                $1999
+                            </td>
+                        </tr>
+                        <tr className="bg-white dark:bg-gray-800">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td className="px-6 py-4">
+                                Black
+                            </td>
+                            <td className="px-6 py-4">
+                                Accessories
+                            </td>
+                            <td className="px-6 py-4">
+                                $99
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+}
--- frontend/src/pages/qbee.tsx
+++ frontend/src/pages/qbee.tsx
@@ -1,29 +1,23 @@
 import Sidebar from "@/components/Navigation/SideBar";
+import { Squares2X2Icon, RectangleStackIcon } from "@heroicons/react/24/solid";
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from 'react';
-import ReactFlow from 'reactflow';
+import { useCallback, useState, useEffect } from 'react';
+import ReactFlow, { applyNodeChanges, MiniMap, Controls, Background, BackgroundVariant, Panel, ReactFlowProvider, ControlButton, useReactFlow, Node, Edge, applyEdgeChanges, OnNodesChange, addEdge, OnEdgesChange, OnConnect, getIncomers, getOutgoers, getConnectedEdges } from 'reactflow';
 import 'reactflow/dist/style.css';
+import { useRouter } from 'next/router';
+import Dagre from '@dagrejs/dagre';
+
+import OutputNode from "@/components/QBee/OutputNode";
+const nodeTypes = { outputNode: OutputNode };
 
 const initialNodes = [
     {
-        id: '1',
-        type: 'input',
+        id: 'outputNode',
+        type: 'outputNode',
         data: { label: 'Input Node' },
         position: { x: 250, y: 25 },
-    },
-
-    {
-        id: '2',
-        // you can also pass a React component as a label
-        data: { label: <div>Default Node</div> },
-        position: { x: 100, y: 125 },
-    },
-    {
-        id: '3',
-        type: 'output',
-        data: { label: 'Output Node' },
-        position: { x: 250, y: 250 },
+        deletable: false
     },
 ];
 
@@ -32,20 +26,115 @@ const initialEdges = [
     { id: 'e2-3', source: '2', target: '3', animated: true },
 ];
 
+const defaultEdgeOptions = { animated: true };
+
+const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
 export default function QBee() {
 
-    const [nodes, setNodes] = useState(initialNodes);
-    const [edges, setEdges] = useState(initialEdges);
+    return <ReactFlowProvider><Flow /></ReactFlowProvider>;
+}
+
+function Flow() {
+
+    const router = useRouter()
+
+    useEffect(() => {
+        onLayout('TB')
+    }, [])
+
+    const [nodes, setNodes] = useState<Node[]>(initialNodes);
+    const [edges, setEdges] = useState<Edge[]>(initialEdges);
+    const { fitView } = useReactFlow();
+
+    const onNodesChange: OnNodesChange = useCallback((changes) => {
+        setNodes((nds) => {
+            const parsedChanges = changes.reduce((res: any, change: any) => {
+                const validChange =
+                    change.type !== 'remove' ||
+                    (change.type === 'remove' && nds.find(n => n.id === change.id)?.data.deletable);
+                if (validChange) {
+                    res.push(change);
+                }
+                return res;
+            }, []);
+
+            return applyNodeChanges(parsedChanges, nds);
+        });
+    }, [setNodes]);
+
+    const onEdgesChange: OnEdgesChange = useCallback(
+        (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [setEdges]
+    );
+
+    const onConnect: OnConnect = useCallback(
+        (connection) => setEdges((eds) => addEdge(connection, eds)),
+        [setEdges]
+    );
+
+    const getLayoutedElements = (nodes: Node[], edges: Edge[], options: any) => {
+        g.setGraph({ rankdir: options.direction });
+
+        edges.forEach((edge: Edge) => g.setEdge(edge.source, edge.target));
+        nodes.forEach((node: Node) => g.setNode(node.id, node));
+
+        Dagre.layout(g);
+
+        return {
+            nodes: nodes.map((node: Node) => {
+                const { x, y } = g.node(node.id);
+
+                return { ...node, position: { x, y } };
+            }),
+            edges,
+        };
+    };
+
+    const onLayout = useCallback(
+        (direction: string) => {
+            const layouted = getLayoutedElements(nodes, edges, { direction });
+
+            setNodes([...layouted.nodes]);
+            setEdges([...layouted.edges]);
+
+            fitView();
+        },
+        [nodes, edges]
+    );
 
     return <>
         <Head>
             <title>QBee</title>
         </Head>
-        <Sidebar />
-        <div className="p-4 sm:ml-64 bg-gray-100 dark:bg-secondaryBackground min-h-screen">
-            <div className="w-full h-full">
-                <ReactFlow nodes={nodes} edges={edges} fitView />
-            </div>
+        <div className="bg-gray-100 dark:bg-secondaryBackground h-screen w-screen">
+            <ReactFlowProvider>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    fitView
+                    defaultEdgeOptions={defaultEdgeOptions}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    proOptions={{
+                        hideAttribution: true
+                    }}
+                    nodeTypes={nodeTypes}
+                >
+                    {/* <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable /> */}
+                    <Controls>
+                        <ControlButton onClick={() => {
+                            onLayout('TB')
+                        }}>
+                            <RectangleStackIcon className="text-black" />
+                        </ControlButton>
+                    </Controls>
+                    <Background color="#ccc" variant={BackgroundVariant.Dots} />
+                    <Panel position="top-right" style={{}} className="bg-black rounded p-2 h-96 w-24">top-right</Panel>
+                    <Panel position="top-left" className="bg-black p-2 rounded-lg cursor-pointer" onClick={() => router.push("/home")}><Squares2X2Icon className="w-4=6 h-6" /></Panel>
+                </ReactFlow>
+            </ReactFlowProvider>
         </div>
     </>
 }
