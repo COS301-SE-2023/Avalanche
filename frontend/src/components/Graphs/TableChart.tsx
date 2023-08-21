67615231e8c105737364e2b7ab000264a15ea2b3
Inge505 <u21689432@tuks.co.za>
2023-08-21 09:03:22 +0200

    Filter
    Still ugly

--- frontend/src/components/Graphs/TableChart.tsx
+++ frontend/src/components/Graphs/TableChart.tsx
@@ -1,49 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import {FunnelIcon} from "@heroicons/react/24/solid";
 
 export function TableChart({ data }: any) {
   data = data.jsonData;
   const headers = data.length ? Object.keys(data[0]) : [];
 
+  // State to hold the selected filters
+  const [filters, setFilters] = useState<any>({});
+
+  // Function to handle filter change
+  const handleFilterChange = (colIndex: number, value: string) => {
+    setFilters({ ...filters, [colIndex]: value });
+  };
+
+  // Filter the data based on the selected filters
+  const filteredData = data.filter((row: any) =>
+    Object.entries(filters).every(
+      ([colIndex, value]) =>
+        value === "" || row[headers[Number(colIndex)]] === value
+    )
+  );
+
+  const [activeFilter, setActiveFilter] = useState<number | null>(null);
+
   return (
     <>
       <div className="relative overflow-x-auto shadow-md sm:rounded border-lightHover">
-        {" "}
         <div className="h-96 overflow-y-auto">
           <table className="w-full h-92 overflow-y-auto border text-sm text-left text-black dark:text-gray-500 dark:text-gray-400 border-gray-400">
-            <thead className="text-xs text-black uppercase bg-gray-300 dark:bg-thirdBackground dark:text-gray-400">
-              {" "}
+          <thead className="text-xs text-black uppercase bg-gray-300 dark:bg-thirdBackground dark:text-gray-400">
               <tr>
                 {headers.map((header, idx) => (
-                  <th
-                    key={idx}
-                    scope="col"
-                    className="px-6 py-3 border border-gray-400"
-                  >
-                    {header}
+                  <th key={idx} scope="col" className="px-6 py-3 border border-gray-400">
+                    {header}{" "}
+                    {idx < headers.length - 1 && (
+                      <button onClick={() => setActiveFilter(idx === activeFilter ? null : idx)}>
+                        <FunnelIcon  className="w-6 h-6 text-right"/>
+                      </button>
+                    )}
                   </th>
                 ))}
               </tr>
             </thead>
+            {activeFilter !== null && (
+              <div className="absolute z-10 bg-white border border-gray-400">
+                <select onChange={(e) => handleFilterChange(activeFilter, e.target.value)}>
+                  <option value="">All</option>
+                  {Array.from(new Set(data.map((row: any) => row[headers[activeFilter]]))).map((value, idx) => (
+                    <option key={idx} value={String(value)}>
+                      {value}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )}
             <tbody>
-              {data.map((row: any, rowIndex: any) => (
+              {filteredData.map((row: any, rowIndex: any) => (
                 <tr
                   key={rowIndex}
-                  className=" border dark:bg-secondaryBackground dark:border-gray-600"
+                  className="border dark:bg-secondaryBackground dark:border-gray-600"
                 >
-                  {" "}
-                  {/* Change here */}
                   {headers.map((header, colIndex) => {
-                    const isNumber = /^-?\d+(\.\d+)?$/.test(row[header]); // Check if it's a number
-                    return  (
-                    <td
-                      key={colIndex}
-                      className="px-6 py-4 dark:text-lightHover border-l border-r border-gray-400 ${typeof row[header] === 'number' ? 'font-bold' : ''} ${isNumber ? 'font-bold' : ''}"
-                    >
-                      {" "}
-                      {/* Change here */}
-                      {row[header]}
-                    </td>
-                  )})}
+                    const isNumber = /^-?\d+(\.\d+)?$/.test(row[header]);
+                    return (
+                      <td
+                        key={colIndex}
+                        className={`px-6 py-4 dark:text-lightHover border-l border-r border-gray-400 ${
+                          isNumber ? "font-bold" : ""
+                        }`}
+                      >
+                        {row[header]}
+                      </td>
+                    );
+                  })}
                 </tr>
               ))}
             </tbody>
