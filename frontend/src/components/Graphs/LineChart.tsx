38b41585ccdd5e3e718a353f54621c0ffe4ee2ee
markbr02 <77788584+markbr02@users.noreply.github.com>
2023-09-05 17:35:55 +0200

    Format data for apex yay

--- frontend/src/components/Graphs/LineChart.tsx
+++ frontend/src/components/Graphs/LineChart.tsx
@@ -2,27 +2,108 @@ import { IChart } from "@/interfaces";
 import React, { useState } from "react";
 import Chart from "react-apexcharts";
 
-export function LineChart({ data, addClass }: IChart) {
-  const [chartData, setChartData] = useState({
-    options: {
-      chart: {
-        id: "basic-bar"
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-      }
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
-      },
-      {
-        name: "series-2",
-        data: [3, 4, 4, 5, 4, 6, 7, 9]
-      }
-    ]
+type JsonDataEntry = {
+  [key: string]: string | number;
+};
+
+type ConvertedData = {
+  series: {
+    name: string;
+    data: number[];
+  }[];
+  options: {
+    xaxis: {
+      categories: string[];
+      title: {
+        text: string;
+      };
+    };
+    yaxis: {
+      title: {
+        text: string;
+      };
+      min: number;
+      max: number;
+    };
+  };
+};
+
+const convertData = (jsonData: JsonDataEntry[]): ConvertedData => {
+  const seriesMap: { [key: string]: { [key: string]: number } } = {};
+  const xAxisSet = new Set<string>();
+  const seriesSet = new Set<string>();
+  let yMin = Infinity;
+  let yMax = -Infinity;
+
+  let xAxisLabel = "";
+  let yAxisLabel = "";
+  let seriesLabel = "";
+
+  if (jsonData.length > 0) {
+    const firstEntryKeys = Object.keys(jsonData[0]);
+    xAxisLabel = firstEntryKeys[0];
+    seriesLabel = firstEntryKeys[1];
+    yAxisLabel = firstEntryKeys[2];
+  }
+
+  // Populate the seriesMap and collect unique xAxis and Series values
+  jsonData.forEach((entry) => {
+    const xAxis = entry[xAxisLabel] as string;
+    const series = entry[seriesLabel] as string;
+    const yAxis = entry[yAxisLabel] as number;
+
+    xAxisSet.add(xAxis);
+    seriesSet.add(series);
+
+    if (!seriesMap[series]) {
+      seriesMap[series] = {};
+    }
+    seriesMap[series][xAxis] = yAxis;
+
+    yMin = Math.min(yMin, yAxis);
+    yMax = Math.max(yMax, yAxis);
   });
+
+  // Initialize the final object
+  const convertedData: ConvertedData = {
+    series: [],
+    options: {
+      xaxis: {
+        categories: Array.from(xAxisSet),
+        title: {
+          text: xAxisLabel,
+        },
+      },
+      yaxis: {
+        title: {
+          text: yAxisLabel,
+        },
+        min: Math.min(0, yMin),
+        max: yMax,
+      },
+    }
+  };
+
+  // Populate the series data
+  seriesSet.forEach((seriesName) => {
+    const data: number[] = [];
+    xAxisSet.forEach((xAxisValue) => {
+      data.push(seriesMap[seriesName][xAxisValue] ?? 0);
+    });
+    convertedData.series.push({
+      name: seriesName,
+      data,
+    });
+  });
+
+  return convertedData;
+};
+
+export function LineChart({ data, addClass }: IChart) {
+
+
+
+  const [chartData, setChartData] = useState(convertData(data.jsonData));
 
   return (
     <div className="line">
