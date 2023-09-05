4582749731ba744a4da2a3bb7c6678f552589656
GT-GillesTeuwen <103629800+GT-GillesTeuwen@users.noreply.github.com>
2023-09-05 14:42:50 +0200

    Classify to pull but bad sorry guys

--- Services/DomainNameAnalysis/domain_name_analysis/src/main/java/avalanche/Core/Classifier.java
+++ Services/DomainNameAnalysis/domain_name_analysis/src/main/java/avalanche/Core/Classifier.java
@@ -13,6 +13,8 @@ import javafx.scene.web.WebHistory.Entry;
 
 public class Classifier {
 
+    private final boolean PRINT_DEBUG = true;
+
     private static List<String> words = new ArrayList<>();
     private static List<String> classifications = new ArrayList<>();
     private static List<Integer> compressedLengths = new ArrayList<>();
@@ -41,22 +43,26 @@ public class Classifier {
 
     private static int k = 6;
 
-    public static String classify(String toClassify) throws IOException {
+    public String classify(String toClassify) throws IOException {
         String x1 = toClassify;
         int Cx1 = compressLength(x1);
 
         // Using a priority queue to store the top k smallest distances
         PriorityQueue<Map.Entry<Integer, Double>> pq = new PriorityQueue<>(
-                (a, b) -> Double.compare(b.getValue(), a.getValue()));
+                (a, b) -> {
+                    return Double.compare(b.getValue(), a.getValue());
+                });
 
         for (int i = 0; i < words.size(); i++) {
 
             String x2 = words.get(i);
             int Cx2 = compressedLengths.get(i);
             String x1x2 = x1 + x2;
+            String x2x1 = x2 + x1;
             int Cx1x2 = compressLength(x1x2);
-
-            double ncdx1x2 = (double) (Cx1x2 - Math.min(Cx1, Cx2)) / Math.max(Cx1, Cx2);
+            int Cx2x1 = compressLength(x1x2);
+            int use = Math.min(Cx1x2, Cx2x1);
+            double ncdx1x2 = (double) (use - Math.min(Cx1, Cx2)) / Math.max(Cx1, Cx2);
             pq.offer(new AbstractMap.SimpleEntry<>(i, ncdx1x2));
             if (pq.size() > k) {
                 pq.poll();
@@ -67,11 +73,20 @@ public class Classifier {
         while (!pq.isEmpty()) {
             Map.Entry<Integer, Double> entry = pq.poll();
             int idx = entry.getKey();
-            String[] wordAndClass = { words.get(idx), words.get(idx) };
+            String[] wordAndClass = { words.get(idx), classifications.get(idx) };
             topKClass.add(new AbstractMap.SimpleEntry<>(wordAndClass, entry.getValue()));
         }
 
         Collections.reverse(topKClass); // Since the smallest distances will be at the beginning
+        if (PRINT_DEBUG) {
+            System.out.print("====\n");
+            for (java.util.Map.Entry<String[], Double> entry : topKClass) {
+                System.out
+                        .println("\u001b[35m" + entry.getKey()[0] + ", " + entry.getKey()[1] + ", [" + entry.getValue()
+                                + "]\u001b[0m");
+            }
+            System.out.println("====");
+        }
         return mostFrequent(topKClass);
 
     }
@@ -80,7 +95,7 @@ public class Classifier {
         Classifier c = new Classifier();
         String word = "hike";
         System.out.println("Classify: " + word + "\u001b[35m");
-        System.out.println("\u001b[33m" + c.classify(word) + "\u001b[0m\n==\n");
+        System.out.println("\u001b[33m" + c.classify(word) + "\u001b[0m\n====\n");
         // String[] spl = word.split(" ");
         // for (String w : spl) {
         // System.out.println("Classify: " + w);
