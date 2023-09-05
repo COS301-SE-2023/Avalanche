e801aa3e73b08e92da2d6d7980eb8c5b152adf49
GT-GillesTeuwen <103629800+GT-GillesTeuwen@users.noreply.github.com>
2023-09-05 20:38:53 +0200

    Did bar,bubble,line,radar

--- frontend/src/components/Graphs/LineChart.tsx
+++ frontend/src/components/Graphs/LineChart.tsx
@@ -1,6 +1,8 @@
 import { IChart } from "@/interfaces";
 import React, { useState } from "react";
 import dynamic from 'next/dynamic'
+import { json } from "stream/consumers";
+import { chartColours } from "./data";
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
@@ -100,21 +102,100 @@ const convertData = (jsonData: JsonDataEntry[]): ConvertedData => {
   return convertedData;
 };
 
-export function LineChart({ data, addClass }: IChart) {
+export function LineChart({ data, height }: IChart) {
 
-  const options = convertData(data.jsonData).options
 
-  const [chartData, setChartData] = useState(convertData(data.jsonData));
+
+  const makeOptions = (jsonData: JsonDataEntry[]) => {
+    let allOptions = convertData(jsonData);
+    Object.assign(allOptions.options, {
+
+      dataLabels: {
+        enabled: false,
+        colors: undefined,  // This will use the series color for each data label
+       
+      },
+      colors:chartColours,
+      stroke: {
+        show: true,
+        curve: 'smooth',
+        lineCap: 'butt',
+        colors: undefined,
+        dashArray: 0, 
+    },
+      chart: {
+        zoom: {
+          enabled: true,
+          type: 'x',
+          autoScaleYaxis: false,
+          zoomedArea: {
+            fill: {
+              color: '#90CAF9',
+              opacity: 0.4
+            },
+            stroke: {
+              color: '#0D47A1',
+              opacity: 0.4,
+              width: 1
+            }
+          }
+        },
+        id: "basic-line",
+        height: 100, // or any other fixed height
+    width: '100%',
+    type: 'area',
+        toolbar: {
+          show: true,
+          tools: {
+            download: false,       // Display the download icon
+            selection: true,      // Display the selection icon
+            zoom: true,           // Display the zoom in and zoom out icons
+            zoomin: false,        // Display only the zoom in icon
+            zoomout: false,       // Display only the zoom out icon
+            pan: false,            // Display the panning icon
+            reset: true           // Display the reset icon
+          }
+        }
+      },
+      tooltip: {
+        theme: 'dark', // or 'light'
+        style: {
+          backgroundColor: 'red', // Background color for the tooltip
+          color: 'white',          // Text color for the tooltip
+          fontSize: '12px',        // Font size for the tooltip
+          fontFamily: 'Arial'     // Font family for the tooltip
+        }
+      },
+      animations: {
+        enabled: true,
+        easing: 'easeinout',
+        speed: 900,
+        animateGradually: {
+            enabled: true,
+            delay: 300
+        },
+        dynamicAnimation: {
+            enabled: true,
+            speed: 350
+        }
+    }
+
+    });
+    return allOptions;
+  }
+
+  const [chartData, setChartData] = useState(makeOptions(data.jsonData));
 
   return (
     <div className="line">
-      <div className="row">
-        <div className="mixed-chart">
+      <div className="row max-h-screen">
+        <div className="mixed-chart max-h-screen">
           {(typeof window !== 'undefined') &&
             <Chart
               options={chartData.options}
               series={chartData.series}
               type="line"
+              height={height}
             />
           }
         </div>
--- frontend/src/components/Graphs/RadarChart.tsx
+++ frontend/src/components/Graphs/RadarChart.tsx
@@ -1,46 +1,205 @@
-import React from "react";
-import {
-  Chart as ChartJS,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Radar } from "react-chartjs-2";
 import { IChart } from "@/interfaces";
-import { chartColours } from "@/components/Graphs/data";
+import React, { useState } from "react";
+import dynamic from 'next/dynamic'
+import { json } from "stream/consumers";
+import { chartColours } from "./data";
 
-ChartJS.register(
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Tooltip,
-  Legend
-);
-const hexToRgba = (hex: string, alpha: number) => {
-    const [r, g, b] = hex.match(/\w\w/g)!.map((x) => parseInt(x, 16));
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+type JsonDataEntry = {
+  [key: string]: string | number;
+};
+
+type ConvertedData = {
+  series: {
+    name: string;
+    data: number[];
+  }[];
+  options: {
+    xaxis: {
+      categories: string[];
+      title: {
+        text: string;
+      };
+    };
+    yaxis: {
+      title: {
+        text: string;
+      };
+      //min: number;
+    };
   };
-  
-  const assignColours = (payload: any) => {
-    const datasets = payload.chartData.datasets;
-  
-    datasets.forEach((set: any, index: number) => {
-      const color = chartColours[index % chartColours.length];
-      set.backgroundColor = hexToRgba(color, 0.6); // Using alpha of 0.6
-      set.borderColor = color; // Using alpha of 0.6
-      set.pointRadius = 4;
-      set.pointHoverRadius = 5;
+};
+
+const convertData = (jsonData: JsonDataEntry[]): ConvertedData => {
+  const seriesMap: { [key: string]: { [key: string]: number } } = {};
+  const xAxisSet = new Set<string>();
+  const seriesSet = new Set<string>();
+  let yMin = Infinity;
+  let yMax = -Infinity;
+
+  let xAxisLabel = "";
+  let yAxisLabel = "";
+  let seriesLabel = "";
+
+  if (jsonData.length > 0) {
+    const firstEntryKeys = Object.keys(jsonData[0]);
+    xAxisLabel = firstEntryKeys[0];
+    seriesLabel = firstEntryKeys[1];
+    yAxisLabel = firstEntryKeys[2];
+  }
+
+  // Populate the seriesMap and collect unique xAxis and Series values
+  jsonData.forEach((entry) => {
+    const xAxis = entry[xAxisLabel] as string;
+    const series = entry[seriesLabel] as string;
+    const yAxis = entry[yAxisLabel] as number;
+
+    xAxisSet.add(xAxis);
+    seriesSet.add(series);
+
+    if (!seriesMap[series]) {
+      seriesMap[series] = {};
+    }
+    seriesMap[series][xAxis] = yAxis;
+
+    yMin = Math.min(yMin, yAxis);
+    yMax = Math.max(yMax, yAxis);
+  });
+
+  // Initialize the final object
+  const convertedData: ConvertedData = {
+    series: [],
+    options: {
+      xaxis: {
+        categories: Array.from(xAxisSet),
+        title: {
+          text: xAxisLabel,
+        },
+      },
+      yaxis: {
+        title: {
+          text: yAxisLabel,
+        },
+        //min: Math.min(0, yMin)
+      },
+
+    }
+  };
+
+  // Populate the series data
+  seriesSet.forEach((seriesName) => {
+    const data: number[] = [];
+    xAxisSet.forEach((xAxisValue) => {
+      data.push(seriesMap[seriesName][xAxisValue] ?? 0);
     });
-  };
-  
-  
+    convertedData.series.push({
+      name: seriesName,
+      data,
+    });
+  });
 
-export function RadarChart({ data }: IChart) {
-  let dataN = JSON.parse(JSON.stringify(data));
-  assignColours(dataN);
-  return <Radar data={dataN.chartData} />;
+  return convertedData;
+};
+
+export function RadarChart({ data, height }: IChart) {
+
+
+
+  const makeOptions = (jsonData: JsonDataEntry[]) => {
+    let allOptions = convertData(jsonData);
+    Object.assign(allOptions.options, {
+
+      dataLabels: {
+        enabled: true,
+        colors: undefined,  // This will use the series color for each data label
+       
+      },
+      colors:chartColours,
+      stroke: {
+        show: true,
+        curve: 'smooth',
+        lineCap: 'butt',
+        colors: undefined,
+        dashArray: 0, 
+    },
+      chart: {
+        zoom: {
+          enabled: true,
+          type: 'x',
+          autoScaleYaxis: false,
+          zoomedArea: {
+            fill: {
+              color: '#90CAF9',
+              opacity: 0.4
+            },
+            stroke: {
+              color: '#0D47A1',
+              opacity: 0.4,
+              width: 1
+            }
+          }
+        },
+        id: "basic-line",
+        height: 100, // or any other fixed height
+    width: '100%',
+    type: 'area',
+        toolbar: {
+          show: true,
+          tools: {
+            download: false,       // Display the download icon
+            selection: true,      // Display the selection icon
+            zoom: true,           // Display the zoom in and zoom out icons
+            zoomin: false,        // Display only the zoom in icon
+            zoomout: false,       // Display only the zoom out icon
+            pan: false,            // Display the panning icon
+            reset: true           // Display the reset icon
+          }
+        }
+      },
+      tooltip: {
+        theme: 'dark', // or 'light'
+        style: {
+          backgroundColor: 'red', // Background color for the tooltip
+          color: 'white',          // Text color for the tooltip
+          fontSize: '12px',        // Font size for the tooltip
+          fontFamily: 'Arial'     // Font family for the tooltip
+        }
+      },
+      animations: {
+        enabled: true,
+        easing: 'easeinout',
+        speed: 900,
+        animateGradually: {
+            enabled: true,
+            delay: 300
+        },
+        dynamicAnimation: {
+            enabled: true,
+            speed: 350
+        }
+    }
+
+    });
+    return allOptions;
+  }
+
+  const [chartData, setChartData] = useState(makeOptions(data.jsonData));
+
+  return (
+    <div className="line">
+      <div className="row max-h-screen">
+        <div className="mixed-chart max-h-screen">
+          {(typeof window !== 'undefined') &&
+            <Chart
+              options={chartData.options}
+              series={chartData.series}
+              type="radar"
+              height={height}
+            />
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
