e6ecc88e34a3f9a804df3bbdf4b037669178f7b1
markbr02 <77788584+markbr02@users.noreply.github.com>
2023-09-07 07:40:01 +0200

    more grafana

--- gateway/src/metrics.controller.ts
+++ gateway/src/metrics.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { Registry, collectDefaultMetrics } from 'prom-client';
 import { httpRequestDurationMicroseconds, httpRequestsTotal } from './user-management/user-mangement.controller';
+import { httpRequestDurationMicroseconds as httpRequestDurationMicrosecondsZARC, httpRequestsTotal as httpRequestsTotalZARC } from './zacr/zacr.controller';
+import { httpRequestDurationMicroseconds as httpRequestDurationMicrosecondsAfrica, httpRequestsTotal as httpRequestsTotalAfrica } from './africa/africa.controller';
+import { httpRequestDurationMicroseconds as httpRequestDurationMicrosecondsRyCE, httpRequestsTotal as httpRequestsTotalRyCE } from './ryce/ryce.controller';
+import { httpRequestDurationMicroseconds as httpRequestDurationMicrosecondsDW, httpRequestsTotal as httpRequestsTotalDW } from './domain-watch/domain-watch.controller';
 
 @Controller('metrics')
 export class MetricsController {
@@ -11,6 +15,14 @@ export class MetricsController {
     collectDefaultMetrics({ register: this.register });
     this.register.registerMetric(httpRequestsTotal);
     this.register.registerMetric(httpRequestDurationMicroseconds);
+    this.register.registerMetric(httpRequestsTotalZARC);
+    this.register.registerMetric(httpRequestDurationMicrosecondsZARC);
+    this.register.registerMetric(httpRequestsTotalAfrica);
+    this.register.registerMetric(httpRequestDurationMicrosecondsAfrica);
+    this.register.registerMetric(httpRequestsTotalRyCE);
+    this.register.registerMetric(httpRequestDurationMicrosecondsRyCE);
+    this.register.registerMetric(httpRequestsTotalDW);
+    this.register.registerMetric(httpRequestDurationMicrosecondsDW);
   }
 
   @Get()
--- gateway/src/ryce/ryce.controller.ts
+++ gateway/src/ryce/ryce.controller.ts
@@ -1,7 +1,24 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, HttpCode, HttpException, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { Counter, Histogram, Registry } from 'prom-client';
 import { lastValueFrom } from 'rxjs';
+
+const register = new Registry();
+export const httpRequestDurationMicroseconds = new Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 0.9, 1],
+});
+
+export const httpRequestsTotal = new Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'code'],
+});
+register.registerMetric(httpRequestDurationMicroseconds);
+register.registerMetric(httpRequestsTotal);
 
 @Controller('ryce')
 export class RyceController {
@@ -10,13 +27,18 @@ export class RyceController {
   @Post('transactions')
   @HttpCode(200)
   async transactions(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'transactions' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'transactions', code: 200 });
+      end({ method: 'POST', route: 'transactions', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'transactions', code: rpcError.status });
+      end({ method: 'POST', route: 'transactions', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
@@ -27,13 +49,18 @@ export class RyceController {
   @Post('transactions-ranking')
   @HttpCode(200)
   async transactionsRaking(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'transactions-ranking' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'transactions-ranking', code: 200 });
+      end({ method: 'POST', route: 'transactions-ranking', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'transactions-ranking', code: rpcError.status });
+      end({ method: 'POST', route: 'transactions-ranking', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
@@ -44,13 +71,18 @@ export class RyceController {
   @Post('marketShare')
   @HttpCode(200)
   async marketShare(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'marketShare' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'marketShare', code: 200 });
+      end({ method: 'POST', route: 'marketShare', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'marketShare', code: rpcError.status });
+      end({ method: 'POST', route: 'marketShare', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
@@ -61,13 +93,18 @@ export class RyceController {
   @Post('age')
   @HttpCode(200)
   async age(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'age' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'age', code: 200 });
+      end({ method: 'POST', route: 'age', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'age', code: rpcError.status });
+      end({ method: 'POST', route: 'age', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
@@ -78,13 +115,40 @@ export class RyceController {
   @Post('domainNameAnalysis/count')
   @HttpCode(200)
   async domainNameAnalysisCount(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'domainNameAnalysis/count' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainNameAnalysis/count', code: 200 });
+      end({ method: 'POST', route: 'domainNameAnalysis/count', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainNameAnalysis/count', code: rpcError.status });
+      end({ method: 'POST', route: 'domainNameAnalysis/count', code: rpcError.status });
+      if (typeof rpcError === 'object') {
+        throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
+      }
+      throw error;
+    }
+  }
+
+  @Post('domainNameAnalysis/classification')
+  @HttpCode(200)
+  async domainNameAnalysisClassification(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
+    const pattern = { cmd: 'domainNameAnalysis/classification' };
+    const payload = data;
+    try {
+      const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainNameAnalysis/classification', code: 200 });
+      end({ method: 'POST', route: 'domainNameAnalysis/classification', code: 200 });
+      return result;
+    } catch (error) {
+      const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainNameAnalysis/classification', code: rpcError.status });
+      end({ method: 'POST', route: 'domainNameAnalysis/classification', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
@@ -95,13 +159,18 @@ export class RyceController {
   @Post('domainNameAnalysis/length')
   @HttpCode(200)
   async domainNameAnalysisLength(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'domainNameAnalysis/length' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainNameAnalysis/length', code: 200 });
+      end({ method: 'POST', route: 'domainNameAnalysis/length', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainNameAnalysis/length', code: rpcError.status });
+      end({ method: 'POST', route: 'domainNameAnalysis/length', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
@@ -112,13 +181,18 @@ export class RyceController {
   @Post('movement/vertical')
   @HttpCode(200)
   async movementVertical(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'movement/vertical' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'movement/vertical', code: 200 });
+      end({ method: 'POST', route: 'movement/vertical', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'movement/vertical', code: rpcError.status });
+      end({ method: 'POST', route: 'movement/vertical', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
@@ -129,13 +203,18 @@ export class RyceController {
   @Post('domainWatchPassive')
   @HttpCode(200)
   async domainWatchPassive(@Body() data: any) {
+    const end = httpRequestDurationMicroseconds.startTimer();
     const pattern = { cmd: 'domainWatchPassive' };
     const payload = data;
     try {
       const result = await lastValueFrom(this.client.send(pattern, payload));
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainWatchPassive', code: 200 });
+      end({ method: 'POST', route: 'domainWatchPassive', code: 200 });
       return result;
     } catch (error) {
       const rpcError = error
+      httpRequestsTotal.inc({ method: 'POST', route: 'domainWatchPassive', code: rpcError.status });
+      end({ method: 'POST', route: 'domainWatchPassive', code: rpcError.status });
       if (typeof rpcError === 'object') {
         throw new HttpException(rpcError.message || 'An unexpected error occurred', rpcError.status || 500);
       }
