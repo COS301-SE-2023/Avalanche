81ad1d42dd8ba4a241c9e9ae8293d6a7bcd9ac93
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-09-08 17:09:45 +0200

    config fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    standalone: true,
-  },
+  // experimental: {
+  //   standalone: true,
+  // },
 }
 
 module.exports = nextConfig
