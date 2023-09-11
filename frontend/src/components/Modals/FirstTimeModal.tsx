26d863aa267c38b7c12829a505ba324bbb6adfc3
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-09-11 21:55:18 +0200

    reorder

--- frontend/src/components/Modals/FirstTimeModal.tsx
+++ frontend/src/components/Modals/FirstTimeModal.tsx
@@ -14,10 +14,10 @@ export default function FirstTimeModal({ close, cookieToSet }: IFirstTimeModal) 
 
     return (
         <ModalWrapper title="Welcome to Avalanche ☃️" cookieToSet={cookieToSet}>
-            <p className="text-lg mb-4 text-gray-500">👋 Hey, we see that this is potentially your first time on Avalanche. Before we get things started, we would just like to confirm your purpose for using Avalanche.</p>
 
             <p className="text-medium mb-4 text-gray-500 italic"><span className='font-bold'>Note from the Skunkworks.</span> Thank you for testing Avalanche. There might be somethings that break, if it does, a report form will pop up. You don't have to fill it in, but it would be greatly appreciated if you do. We do make use of <Anchor href="https://sentry.io" text="Sentry.io" /> to log errors automatically when they happen. Any information/data that is collected will only be used for the purpose of testing of our application (like if we need to follow up on a issue/bug you encountered). <Anchor href="https://sentry.io/privacy/" text="Sentry.io Privacy Policy" /></p>
 
+            <p className="text-lg mb-4 text-gray-500">👋 Hey, we see that this is potentially your first time on Avalanche. Before we get things started, we would just like to confirm your purpose for using Avalanche.</p>
             <div className="flex gap-5">
                 <SubmitButton text="I am here as a Registrar" onClick={() => {
                     router.push("/settings?tab=integrations");
