954bd24265d3d6adb482b1ed99473f0d2a6366d2
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-09-14 11:11:08 +0200

    removing error handling

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,20 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useEffect } from 'react'
 
 export default function Document() {
-
-  useEffect(() => {
-    const errorHandler = (event: any) => {
-      console.error('Unhandled Error:', event.error);
-      // You can also display a user-friendly message here.
-    };
-
-    window.addEventListener('error', errorHandler);
-
-    return () => {
-      window.removeEventListener('error', errorHandler);
-    };
-  }, []);
 
   return (
     <Html lang="en">
