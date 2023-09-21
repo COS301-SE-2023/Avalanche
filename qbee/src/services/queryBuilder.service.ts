370e9ac323fa8a688ddd94582df1764531dbce3e
Inge505 <u21689432@tuks.co.za>
2023-09-21 10:22:03 +0200

    Just formatting

--- qbee/src/services/queryBuilder.service.ts
+++ qbee/src/services/queryBuilder.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { JSONQuery } from '../interfaces/JSONQuery';
-import { PermissionsService } from './permission.service';
 import { SQLTranslatorService } from './sqlTranslator.service';
 import Redis from 'ioredis';
 import { SchemaService } from './schema.service';
@@ -10,20 +9,27 @@ export class QueryBuilderService {
   constructor(
     @Inject('REDIS') private readonly redis: Redis,
     private readonly sqlTranslator: SQLTranslatorService,
-    private readonly schemaService: SchemaService, 
+    private readonly schemaService: SchemaService,
   ) {}
 
-  async constructQuery(query: JSONQuery, token: string, schema: string, dataSource: string): Promise<any> {
+  async constructQuery(
+    query: JSONQuery,
+    token: string,
+    schema: string,
+    dataSource: string,
+  ): Promise<any> {
     //First check permissions
     const user = await this.redis.get(token);
     const userDetails = JSON.parse(user);
-    if(dataSource == 'zacr'){
-      dataSource = 'zarc'
+    if (dataSource == 'zacr') {
+      dataSource = 'zarc';
     }
-    const permissionsForDataSource = userDetails.products.filter((product) => product.dataSource === dataSource);
+    const permissionsForDataSource = userDetails.products.filter(
+      (product) => product.dataSource === dataSource,
+    );
     const schemaUrl = `/${dataSource}/${schema}`;
     const schemaDetail = this.schemaService.getSchemaByUrl(schemaUrl);
-    
+
     if (!schemaDetail) {
       return {
         status: 404,
@@ -32,10 +38,14 @@ export class QueryBuilderService {
         timestamp: new Date().toISOString(),
       };
     }
-    
+
     // Validate user access against the schema
-    const hasAccess = this.schemaService.validateUserAccess(permissionsForDataSource[0].tou, query, schemaDetail);  // Assuming permission has the required info
-    
+    const hasAccess = this.schemaService.validateUserAccess(
+      permissionsForDataSource[0].tou,
+      query,
+      schemaDetail,
+    ); // Assuming permission has the required info
+
     if (!hasAccess) {
       return {
         status: 403,
@@ -44,7 +54,6 @@ export class QueryBuilderService {
         timestamp: new Date().toISOString(),
       };
     }
-
 
     //Build SQL query
     const SQLquery = this.sqlTranslator.generateSQL(query);
